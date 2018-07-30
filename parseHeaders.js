365beb9f2df877ceef63fdf4abf28b9c58e50462
Ryder Dale <rydersdale@gmail.com>
2018-07-30 13:51:53 -0500

    exercise complete

--- parseHeaders.js
+++ parseHeaders.js
@@ -2,9 +2,20 @@
 
 /* eslint-env node */
 
-function parseHeaders(text) {
-    // your code here
-    return text;
-}
+function parseHeaders(string) {
+    let stringArr = string.split("\n");
+    const [version, code] = stringArr.shift().split(' ');
+    const returnObject = {version, code, headers: {}};
+    while (stringArr.length > 0) {
+        let intraArray = stringArr.shift().split(': ', 2);
+        if(intraArray.length !== 2) {
+            break;
+        }
+        returnObject['headers'][intraArray[0]] = intraArray[1];
+    }
+    returnObject.body = stringArr.join()
+    console.log(returnObject);
+    return returnObject;
+};
 
 module.exports = parseHeaders;
